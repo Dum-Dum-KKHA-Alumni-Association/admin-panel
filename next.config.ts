47687f96ae4f768cde5c27b648577f3b8b207f09
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-01-03 21:21:59 +0530

    refactor: remove check eslint

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,32 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 	/* config options here */
 	async headers() {
 		return [
-			// {
-			// 	// Apply to all API routes
-			// 	source: '/api/:path*',
-			// 	headers: [
-			// 		{
-			// 			key: 'Access-Control-Allow-Origin',
-			// 			value: 'http://localhost:3000', // Specify your allowed origin
-			// 		},
-			// 		{
-			// 			key: 'Access-Control-Allow-Methods',
-			// 			value: 'GET, POST, OPTIONS', // Allowed HTTP methods
-			// 		},
-			// 		{
-			// 			key: 'Access-Control-Allow-Headers',
-			// 			value: 'Content-Type, Authorization', // Allowed headers
-			// 		},
-			// 	],
-			// },
+			{
+				// Apply to all API routes
+				source: '/api/:path*',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: process.env.NEXT_PUBLIC_FRONTEND_URL!, // Specify your allowed origin
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET, POST, OPTIONS', // Allowed HTTP methods
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'Content-Type, Authorization', // Allowed headers
+					},
+				],
+			},
 			// {
 			// 	// Apply to all API routes
 			// 	source: '/api/:path*',
