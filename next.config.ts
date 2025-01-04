c2b8fbbe9bc7e1ff4079ab86b612c1181ce3f049
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-01-05 01:32:10 +0530

    feat: update middlewate

--- next.config.ts
+++ next.config.ts
@@ -15,8 +15,12 @@ const nextConfig: NextConfig = {
 				headers: [
 					{
 						key: 'Access-Control-Allow-Origin',
-						value: process.env.NEXT_PUBLIC_FRONTEND_URL!, // Specify your allowed origin
+						value: `${process.env.NEXT_PUBLIC_FRONTEND_URL}`, // Specify your allowed origin
 					},
+					// {
+					// 	key: 'Access-Control-Allow-Origin',
+					// 	value: 'https://www.ddkkhaaa.org', // Specify your allowed origin
+					// },
 					{
 						key: 'Access-Control-Allow-Methods',
 						value: 'GET, POST, OPTIONS', // Allowed HTTP methods
@@ -27,24 +31,6 @@ const nextConfig: NextConfig = {
 					},
 				],
 			},
-			// {
-			// 	// Apply to all API routes
-			// 	source: '/api/:path*',
-			// 	headers: [
-			// 		{
-			// 			key: 'Access-Control-Allow-Origin',
-			// 			value: 'http://localhost:8080', // Specify your allowed origin
-			// 		},
-			// 		{
-			// 			key: 'Access-Control-Allow-Methods',
-			// 			value: 'GET, POST, OPTIONS', // Allowed HTTP methods
-			// 		},
-			// 		{
-			// 			key: 'Access-Control-Allow-Headers',
-			// 			value: 'Content-Type, Authorization', // Allowed headers
-			// 		},
-			// 	],
-			// },
 		];
 	},
 };
